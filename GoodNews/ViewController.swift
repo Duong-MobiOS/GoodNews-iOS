fa793cbce7073498d0672875705398f3e8f5d223
DuongMt1 <duongmt1@fsoft.con.vn>
2022-05-13 14:03:41 +0700

    Initial Commit

---
+++ GoodNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodNews
+//
+//  Created by DuongMT1.TCS on 5/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
